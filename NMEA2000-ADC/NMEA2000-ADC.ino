20dfd1d7afe2cbdb407a78bd3a3cf8badd6d27e5
AK-Homberger <53138495+AK-Homberger@users.noreply.github.com>
2021-01-08 13:37:15 +0100

    Add files via upload

---
+++ NMEA2000-ADC/NMEA2000-ADC.ino
@@ -0,0 +1,211 @@
+/*
+  This code is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  This code is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// NMEA2000 Spannung und Widerstand mit ADC
+// Version 0.1, 08.01.2021, AK-Homberger
+
+#define ESP32_CAN_TX_PIN GPIO_NUM_5  // Set CAN TX port to 5 
+#define ESP32_CAN_RX_PIN GPIO_NUM_4  // Set CAN RX port to 4
+
+#include <Arduino.h>
+#include <Preferences.h>
+#include <NMEA2000_CAN.h>  // This will automatically choose right CAN library and create suitable NMEA2000 object
+#include <N2kMessages.h>
+
+
+#define ADCpin1 34                   // GPIO 34 (Analog input, ADC1_CH6)
+#define ADC_Calibration_Value1 250.0 // For resistor measure 5 Volt and 180 Ohm equals 100% plus 1K resistor.
+
+#define ADCpin2 35                   // GPIO 35 (Analog input, ADC1_CH7))
+#define ADC_Calibration_Value2 34.3  // To measure battery voltage. The real value depends on the true resistor values for the ADC input (100K / 27 K).
+
+
+// Set time offsets
+#define SlowDataUpdatePeriod 1000  // Time between CAN Messages sent
+#define BatteryVoltageSendOffset 0
+#define WaterTankLevelSendOffset 50
+
+
+int NodeAddress;            // To store last Node Address
+Preferences preferences;    // Nonvolatile storage on ESP32 - To store LastDeviceAddress
+
+// Set the information for other bus devices, which messages we support
+const unsigned long TransmitMessages[] PROGMEM = {127508L, // Battery Status
+                                                  127505L, // Fluid level
+                                                  0
+                                                 };
+
+
+//*****************************************************************************
+void setup() {
+  uint8_t chipid[6];
+  uint32_t id = 0;
+  int i = 0;
+
+  // Init USB serial port
+  Serial.begin(115200);
+  delay(10);
+  
+  // Reserve enough buffer for sending all messages.
+  NMEA2000.SetN2kCANMsgBufSize(8);
+  NMEA2000.SetN2kCANReceiveFrameBufSize(150);
+  NMEA2000.SetN2kCANSendFrameBufSize(150);
+
+  // Generate unique number from chip id
+  esp_efuse_read_mac(chipid);
+  for (i = 0; i < 6; i++) id += (chipid[i] << (7 * i));
+
+  // Set product information
+  NMEA2000.SetProductInformation("1", // Manufacturer's Model serial code
+                                 100, // Manufacturer's product code
+                                 "My Sensor Module",  // Manufacturer's Model ID
+                                 "1.0.2.25 (2019-07-07)",  // Manufacturer's Software version code
+                                 "1.0.2.0 (2019-07-07)" // Manufacturer's Model version
+                                );
+  // Set device information
+  NMEA2000.SetDeviceInformation(id, // Unique number. Use e.g. Serial number.
+                                130, // Device function=Temperature. See codes on http://www.nmea.org/Assets/20120726%20nmea%202000%20class%20&%20function%20codes%20v%202.00.pdf
+                                75, // Device class=Sensor Communication Interface. See codes on  http://www.nmea.org/Assets/20120726%20nmea%202000%20class%20&%20function%20codes%20v%202.00.pdf
+                                2046 // Just choosen free from code list on http://www.nmea.org/Assets/20121020%20nmea%202000%20registration%20list.pdf
+                               );
+
+  preferences.begin("nvs", false);                          // Open nonvolatile storage (nvs)
+  NodeAddress = preferences.getInt("LastNodeAddress", 34);  // Read stored last NodeAddress, default 34
+  preferences.end();
+  Serial.printf("NodeAddress=%d\n", NodeAddress);
+
+  // If you also want to see all traffic on the bus use N2km_ListenAndNode instead of N2km_NodeOnly below
+  NMEA2000.SetMode(tNMEA2000::N2km_NodeOnly, NodeAddress);
+  NMEA2000.ExtendTransmitMessages(TransmitMessages);
+
+  NMEA2000.Open();
+
+  delay(200);
+}
+
+
+//*****************************************************************************
+// ReadADC is used to improve the linearity of the ESP32 ADC see: https://github.com/G6EJD/ESP32-ADC-Accuracy-Improvement-function
+double ReadADC(byte pin) {
+  double reading = analogRead(pin); // Reference voltage is 3v3 so maximum reading is 3v3 = 4095 in range 0 to 4095
+  if (reading < 1 || reading > 4095) return 0;
+  // return -0.000000000009824 * pow(reading,3) + 0.000000016557283 * pow(reading,2) + 0.000854596860691 * reading + 0.065440348345433;
+  return (-0.000000000000016 * pow(reading, 4) + 0.000000000118171 * pow(reading, 3) - 0.000000301211691 * pow(reading, 2) + 0.001109019271794 * reading + 0.034143524634089) * 1000;
+} // Added an improved polynomial, use either, comment out as required
+
+
+// Functions to control send interval and time offets
+
+//*****************************************************************************
+bool IsTimeToUpdate(unsigned long NextUpdate) {
+  return (NextUpdate < millis());
+}
+
+
+//*****************************************************************************
+unsigned long InitNextUpdate(unsigned long Period, unsigned long Offset = 0) {
+  return millis() + Period + Offset;
+}
+
+
+//*****************************************************************************
+void SetNextUpdate(unsigned long & NextUpdate, unsigned long Period) {
+  while ( NextUpdate < millis() ) NextUpdate += Period;
+}
+
+
+//*****************************************************************************
+void SendN2kBatteryVoltage(void) {
+  static unsigned long SlowDataUpdated = InitNextUpdate(SlowDataUpdatePeriod, BatteryVoltageSendOffset);
+  tN2kMsg N2kMsg;
+  double BatteryVoltage;
+
+  if ( IsTimeToUpdate(SlowDataUpdated) ) {
+    SetNextUpdate(SlowDataUpdated, SlowDataUpdatePeriod);
+
+    BatteryVoltage = ReadADC(ADCpin2) * ADC_Calibration_Value2 / 4096;
+    
+    Serial.printf("Battery Voltage: %3.1f V \n", BatteryVoltage);
+
+    // Definition from N2kMessages.h
+    // void SetN2kPGN127508(tN2kMsg &N2kMsg, unsigned char BatteryInstance, double BatteryVoltage, double BatteryCurrent=N2kDoubleNA,
+    //                 double BatteryTemperature=N2kDoubleNA, unsigned char SID=1);
+
+    // Set N2K message
+    SetN2kPGN127508(N2kMsg, 0, BatteryVoltage, N2kDoubleNA, N2kDoubleNA, 0);
+        
+    // Send message
+    NMEA2000.SendMsg(N2kMsg);
+  }
+}
+
+
+//*****************************************************************************
+void SendN2kWaterTankLevel(void) {
+  static unsigned long SlowDataUpdated = InitNextUpdate(SlowDataUpdatePeriod, WaterTankLevelSendOffset);
+  tN2kMsg N2kMsg;
+  double TankLevel;
+
+  if ( IsTimeToUpdate(SlowDataUpdated) ) {
+    SetNextUpdate(SlowDataUpdated, SlowDataUpdatePeriod);
+
+    TankLevel = ReadADC(ADCpin1) * ADC_Calibration_Value1 / 4096;
+    
+    Serial.printf("Tank Level: %3.1f %% \n", TankLevel);
+
+    // Definition from N2kMessages.h
+    // void SetN2kPGN127505(tN2kMsg &N2kMsg, unsigned char Instance, tN2kFluidType FluidType, double Level, double Capacity);
+
+    // FluidType is defined in N2kTypes.h
+
+    // Set N2K message
+    SetN2kPGN127505(N2kMsg, 0, N2kft_Water, TankLevel, 100);
+    
+    // Send message
+    NMEA2000.SendMsg(N2kMsg);
+  }
+}
+
+
+//*****************************************************************************
+// Function to check if SourceAddress has changed (due to address conflict on bus)
+
+void CheckSourceAddressChange() {
+  int SourceAddress = NMEA2000.GetN2kSource();
+
+  if (SourceAddress != NodeAddress) { // Save potentially changed Source Address to NVS memory
+    preferences.begin("nvs", false);
+    preferences.putInt("LastNodeAddress", SourceAddress);
+    preferences.end();
+    Serial.printf("Address Change: New Address=%d\n", SourceAddress);
+  }
+}
+
+
+//*****************************************************************************
+void loop() {
+
+  SendN2kBatteryVoltage();
+  
+  SendN2kWaterTankLevel();
+
+  NMEA2000.ParseMessages();
+
+  CheckSourceAddressChange();
+
+  // Dummy to empty input buffer to avoid board to stuck with e.g. NMEA Reader
+  if ( Serial.available() ) {
+    Serial.read();
+  }
+}
